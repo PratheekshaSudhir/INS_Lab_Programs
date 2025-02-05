214eca0d31c182d65efd99393c8abad002d46edf
PratheekshaSudhir <151824934+PratheekshaSudhir@users.noreply.github.com>
2025-02-05 05:43:36 +0000

    update didn't work

--- Lab4_Hill_Cipher/hill_cipher.py
+++ Lab4_Hill_Cipher/hill_cipher.py
@@ -9,6 +9,7 @@ def numbers_to_text(numbers):
 def encrypt_hill(plain,key_mat):
     cipher_num=[]
     n=len(key_mat)
+    print(n)
     plain=plain.upper().replace(" ","")
     while (len(plain)%n !=0):
         plain=plain+"X"
@@ -22,13 +23,16 @@ def encrypt_hill(plain,key_mat):
     return numbers_to_text(cipher_num)
 
 plain=input("enter the plain text to encrypt ")
-# key_text=input("enter the key")
-# m=int(input("Enter the dimension of the key"))
-# key_mat=[]
-# while(len(key_text)%m !=0):
-#     key_text+="X"
+#key_text=input("enter the key")
+#m=int(input("Enter the dimension of the key"))
+#while(len(key_text)% (m*m)  !=0):
+#    key_text+="X"
+#key1=text_to_numbers(key_text)
+#key_mat=np.array(key1).reshape(m, m)
+#print(key_mat)
 # for i in range(0,len(key_text),m):
 #     key_vector=np.array(key_text[i:i+m])
 #     key_mat.extend(key_vector)
 key=[[7,8],[11,11]]
-print("Cipher Text: ",encrypt_hill(plain,key))
+print("Cipher Text: ",encrypt_hill(plain,key
+                                   ))
