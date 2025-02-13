2458509d45fc9549720b491a8fa32a0b160b9ef0
PratheekshaSudhir <151824934+PratheekshaSudhir@users.noreply.github.com>
2025-02-13 16:06:31 +0000

    added decrypt function

--- Lab4_Hill_Cipher/hill_cipher.py
+++ Lab4_Hill_Cipher/hill_cipher.py
@@ -9,7 +9,6 @@ def numbers_to_text(numbers):
 def encrypt_hill(plain,key_mat):
     cipher_num=[]
     n=len(key_mat)
-    print(n)
     plain=plain.upper().replace(" ","")
     while (len(plain)%n !=0):
         plain=plain+"X"
@@ -22,16 +21,45 @@ def encrypt_hill(plain,key_mat):
         
     return numbers_to_text(cipher_num)
 
-plain=input("enter the plain text to encrypt ")
-#key_text=input("enter the key")
-#m=int(input("Enter the dimension of the key"))
-#while(len(key_text)% (m*m)  !=0):
+def mod_inverse_matrix(matrix, mod):
+    det = int(np.round(np.linalg.det(matrix)))  # Compute determinant
+    det_inv = pow(det, -1, mod)  # Modular inverse of determinant
+    matrix_adj = np.round(det * np.linalg.inv(matrix)).astype(int) % mod  # Adjugate matrix
+    return (det_inv * matrix_adj) % mod  # Inverse matrix mod 26
+
+def decrypt_hill(cipher, key_mat):
+    plain_num = []
+    n = len(key_mat)
+    
+    cipher = cipher.upper().replace(" ", "")
+    
+    key_inv = mod_inverse_matrix(np.array(key_mat), 26)  # Compute modular inverse of key
+    
+    cipher_num = text_to_numbers(cipher)
+    
+    for i in range(0, len(cipher_num), n):
+        vector = np.array(cipher_num[i:i+n])
+        plain_inter = np.dot(key_inv, vector) % 26
+        plain_num.extend(plain_inter)
+
+    return numbers_to_text(plain_num[:leng])
+
+plain=input("enter the plain text ")
+leng=len(plain)
+# key_text=input("enter the key")
+# m=int(input("Enter the dimension of the key"))
+# while(len(key_text)% (m*m)  !=0):
 #    key_text+="X"
-#key1=text_to_numbers(key_text)
-#key_mat=np.array(key1).reshape(m, m)
-#print(key_mat)
+# key1=text_to_numbers(key_text)
+# key_mat=np.array(key1).reshape(m, m)
+# print(key_mat)
 # for i in range(0,len(key_text),m):
 #     key_vector=np.array(key_text[i:i+m])
 #     key_mat.extend(key_vector)
-key=[[7,8],[11,11]]
+key = [[7, 8], [11, 11]]
+cipher=encrypt_hill(plain,key)
 print("Cipher Text: ",encrypt_hill(plain,key))
+print("Plain Text: ", decrypt_hill(cipher, key))
+
+
+
